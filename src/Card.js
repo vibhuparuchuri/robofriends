2603842e108863bf287e3135cea366e4fab39e47
Vibhu <vibhu96@gmail.com>
2023-07-03 21:45:21 -0700

    changed card image size

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     const {name,email,id} = props;
     return (
         <div className='tc bg-light-pink dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src={`https://robohash.org/${id}?size=200x200`}></img>
+            <img alt='robots' src={`https://robohash.org/${id}?size=300x300`}></img>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
